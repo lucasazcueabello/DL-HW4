17cf40879a4cc7a82c256793559816c5ee783ae2
lucasazcueabello <lucasazcueabello@gmail.com>
2025-04-07 14:40:28 -0400

    working all three models 98/100 on local tests

--- models.py
+++ models.py
@@ -24,6 +24,21 @@ class MLPPlanner(nn.Module):
         self.n_track = n_track
         self.n_waypoints = n_waypoints
 
+        hidden_values = 25
+
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=self.n_track*4, out_features=hidden_values),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_values, out_features=hidden_values),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_values, out_features=hidden_values),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_values, out_features=hidden_values),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_values, out_features=self.n_waypoints*2)
+        )
+
     def forward(
         self,
         track_left: torch.Tensor,
@@ -43,7 +58,7 @@ class MLPPlanner(nn.Module):
         Returns:
             torch.Tensor: future waypoints with shape (b, n_waypoints, 2)
         """
-        raise NotImplementedError
+        return self.layer_stack(torch.cat([track_left, track_right], dim=1)).reshape(-1,3,2)
 
 
 class TransformerPlanner(nn.Module):
@@ -59,6 +74,12 @@ class TransformerPlanner(nn.Module):
         self.n_waypoints = n_waypoints
 
         self.query_embed = nn.Embedding(n_waypoints, d_model)
+        self.input_embed = nn.Linear(in_features=2, out_features=d_model)
+        self.cross_attention = nn.MultiheadAttention(embed_dim=d_model, num_heads=8, batch_first=True)
+        self.self_attention = nn.TransformerDecoder(nn.TransformerDecoderLayer(d_model=d_model, nhead=8, dim_feedforward=200, batch_first=True), num_layers=1)
+        self.output_embed = nn.Linear(in_features=64, out_features=2)
+        self.norm = nn.LayerNorm(d_model)
+
 
     def forward(
         self,
@@ -79,13 +100,40 @@ class TransformerPlanner(nn.Module):
         Returns:
             torch.Tensor: future waypoints with shape (b, n_waypoints, 2)
         """
-        raise NotImplementedError
+        left = self.input_embed(track_left)
+        right = self.input_embed(track_right)
+        if torch.isnan(left).any():
+            print("Nan element left")
+            print(torch.isnan(left).any(dim=(1, 2)))
+            print(left)
+        if torch.isnan(right).any():
+            print("Nan element right")
+        x = torch.cat([left, right], dim=1)
+        if torch.isnan(x).any():
+            print("Nan element x")
+        k = self.query_embed.weight.unsqueeze(0).expand(x.shape[0], -1, -1)
+        if torch.isnan(k).any():
+            print("Nan in k")
+        attn_output, _ = self.cross_attention(k, x, x)
+        attn_output = self.norm(attn_output) 
+        y = self.self_attention(attn_output, attn_output)
+        #if torch.isnan(y2).any():
+            #nan_positions = torch.nonzero(torch.isnan(y2), as_tuple=False)
+            #for pos in nan_positions:
+                #print(tuple(pos.tolist()))
+                #print(y1[pos])
+            #print("Nan element y2")
+        #y = torch.nan_to_num(y, nan=0.5)
+        
+        return self.output_embed(y)
 
 
 class CNNPlanner(torch.nn.Module):
     def __init__(
         self,
         n_waypoints: int = 3,
+        in_channels: int = 3,
+        num_blocks: int = 7
     ):
         super().__init__()
 
@@ -93,6 +141,20 @@ class CNNPlanner(torch.nn.Module):
 
         self.register_buffer("input_mean", torch.as_tensor(INPUT_MEAN), persistent=False)
         self.register_buffer("input_std", torch.as_tensor(INPUT_STD), persistent=False)
+
+        cnn_layers = [
+            #torch.nn.Conv2d(3, in_channels, kernel_size=7, stride=2, padding=3),
+            #torch.nn.GELU(),
+        ]
+        c1 = in_channels
+        for _ in range(num_blocks):
+            c2 = c1 * 2
+            cnn_layers.append(self.Block(c1, c2, stride=2))
+            c1 = c2
+        cnn_layers.append(torch.nn.Conv2d(c1, self.n_waypoints*2, kernel_size=1))
+        cnn_layers.append(torch.nn.AdaptiveAvgPool2d(1))
+        self.network = torch.nn.Sequential(*cnn_layers)
+        pass
 
     def forward(self, image: torch.Tensor, **kwargs) -> torch.Tensor:
         """
@@ -103,10 +165,34 @@ class CNNPlanner(torch.nn.Module):
             torch.FloatTensor: future waypoints with shape (b, n, 2)
         """
         x = image
-        x = (x - self.input_mean[None, :, None, None]) / self.input_std[None, :, None, None]
+        z = (x - self.input_mean[None, :, None, None]) / self.input_std[None, :, None, None]
+        logits = self.network(z)
+        return logits.squeeze().reshape(logits.shape[0], self.n_waypoints,2)
 
-        raise NotImplementedError
+    class Block(torch.nn.Module):
+        def __init__(self, in_channels, out_channels, stride):
+            super().__init__()
+            kernel_size = 3
+            padding = (kernel_size - 1) // 2
 
+            self.c1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+            self.c2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size, 1, padding)
+            self.c3 = torch.nn.Conv2d(out_channels, out_channels, kernel_size, 1, padding)
+            self.gelu = torch.nn.GELU()
+            self.batch_norm = torch.nn.BatchNorm2d(out_channels)
+            self.dropout = torch.nn.Dropout(p=0.1)
+
+            if in_channels != out_channels or stride != 1:
+                self.residual = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0)
+            else:
+                self.residual = nn.Identity()
+
+        def forward(self, x):
+            x1 = self.dropout(self.gelu(self.batch_norm(self.c1(x))))
+            x1 = self.dropout(self.gelu(self.batch_norm(self.c2(x1))))
+            x1 = self.dropout(self.gelu(self.batch_norm(self.c3(x1))))
+            x1 = self.dropout(self.gelu(self.batch_norm(self.c3(x1))))
+            return x1 + self.residual(x)
 
 MODEL_FACTORY = {
     "mlp_planner": MLPPlanner,
--- train_planner.py
+++ train_planner.py
@@ -3,4 +3,165 @@ Usage:
     python3 -m homework.train_planner --your_args here
 """
 
-print("Time to train")
+import argparse
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch.utils.tensorboard as tb
+
+from .models import  load_model, save_model
+from .datasets.road_dataset import load_data
+from .metrics import PlannerMetric
+
+from torchview import draw_graph
+
+def train(
+    exp_dir: str = "logs",
+    model_name: str = "mlp_planner",
+    num_epoch: int = 50,
+    lr: float = 1e-3,
+    batch_size: int = 64,
+    seed: int = 2024,
+    **kwargs,
+):
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        print("CUDA not available, using CPU")
+        device = torch.device("cpu")
+
+    # set random seed so each run is deterministic
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    # directory with timestamp to save tensorboard logs and model checkpoints
+    log_dir = Path(exp_dir) / f"{model_name}_{datetime.now().strftime('%m%d_%H%M%S')}"
+    logger = tb.SummaryWriter(log_dir)
+
+    # note: the grader uses default kwargs, you'll have to bake them in for the final submission
+    model = load_model(model_name, **kwargs)
+    print(model)
+    #model_graph = draw_graph(model=model, device="meta")
+    #model_graph.visual_graph
+    model = model.to(device)
+    model.train()
+
+    train_data = load_data("drive_data/train", shuffle=True, transform_pipeline="default", batch_size=batch_size, num_workers=2)
+    val_data = load_data("drive_data/val", shuffle=False)
+
+    # create loss function and optimizer
+    if model_name == "transformer_planner":
+        loss_func = torch.nn.HuberLoss()
+    else:
+        loss_func = torch.nn.SmoothL1Loss()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+    #optimizer = torch.optim.Adam()
+
+    global_step = 0
+    metrics = {"train_err": PlannerMetric(), "val_err": PlannerMetric()}
+    min_err = 1000.0
+
+    # training loop
+    for epoch in range(num_epoch):
+        # clear metrics at beginning of epoch
+        for key in metrics:
+            metrics[key].reset()
+
+        model.train()
+        #TODO: see what returns from the data loader
+        for batch in train_data:
+            track_left, track_right, waypoints, waypoints_mask = batch["track_left"].to(device), batch["track_right"].to(device), batch["waypoints"].to(device), batch["waypoints_mask"].to(device)
+
+            # TODO: implement training step
+            with torch.autograd.detect_anomaly():
+                preds = model(track_left, track_right)
+            error = loss_func(preds, waypoints)
+            if torch.isnan(error):
+                print("Nan in error")
+                return
+            error_masked = error * waypoints_mask[..., None]
+
+            longitudinal_error = error_masked[:,0].sum() / ( waypoints_mask.sum().float() + 1e-8)
+            lateral_error = error_masked[:,1].sum() / (waypoints_mask.sum().float() + 1e-8)
+            l1_error = longitudinal_error + lateral_error
+
+
+            metrics["train_err"].add(preds, waypoints, waypoints_mask)
+            
+            optimizer.zero_grad()
+            l1_error.backward()
+            optimizer.step()
+
+            global_step += 1
+
+        # disable gradient computation and switch to evaluation mode
+        with torch.inference_mode():
+            model.eval()
+
+            for batch in val_data:
+                track_left, track_right, waypoints, waypoints_mask = batch["track_left"].to(device), batch["track_right"].to(device), batch["waypoints"].to(device), batch["waypoints_mask"].to(device)
+
+                # TODO: compute validation accuracy
+                test_pred = model(track_left, track_right)
+
+                metrics["val_err"].add(test_pred, waypoints, waypoints_mask)
+
+
+        # log average train and val accuracy to tensorboard
+        epoch_train_err = torch.as_tensor(metrics["train_err"].compute()["l1_error"])
+        epoch_val_err = torch.as_tensor(metrics["val_err"].compute()["l1_error"])
+
+        epoch_train_long_err = torch.as_tensor(metrics["train_err"].compute()["longitudinal_error"])
+        epoch_val_long_err = torch.as_tensor(metrics["val_err"].compute()["longitudinal_error"])
+
+        epoch_train_lat_err = torch.as_tensor(metrics["train_err"].compute()["lateral_error"])
+        epoch_val_lat_err = torch.as_tensor(metrics["val_err"].compute()["lateral_error"])
+
+        logger.add_scalar('train_l1_error', (torch.tensor(metrics["train_err"].compute()["l1_error"])).item(), global_step)
+        logger.add_scalar('val_l1_error', (torch.tensor(metrics["val_err"].compute()["l1_error"])).item(), global_step)
+
+        # print on first, last, every 10th epoch
+        if epoch == 0 or epoch == num_epoch - 1 or (epoch + 1) % 10 == 0:
+            print(
+                f"Epoch {epoch + 1:2d} / {num_epoch:2d}: "
+                f"train_l1_error={epoch_train_err:.4f} "
+                f"val_l1_error={epoch_val_err:.4f}\n"
+                f"train_longitudinal_error={epoch_train_long_err:.4f} "
+                f"val_longitudinal_error={epoch_val_long_err:.4f}\n"
+                f"train_lateral_error={epoch_train_lat_err:.4f} "
+                f"val_lateral_error={epoch_val_lat_err:.4f}\n"
+            )
+            if(epoch_val_err < min_err):
+                min_err = epoch_val_err
+                torch.save(model.state_dict(), log_dir / f"{model_name}-earlystop.th")
+                print(f"Model saved to {log_dir / f'{model_name}-earlystop.th'}")
+
+    # save a copy of model weights in the log directory
+    torch.save(model.state_dict(), log_dir / f"{model_name}.th")
+    print(f"Model saved to {log_dir / f'{model_name}.th'}")
+
+    # save and overwrite the model in the root directory for grading
+    model.load_state_dict(torch.load(log_dir / f"{model_name}-earlystop.th", weights_only=True))
+    model.eval()
+    save_model(model)
+
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--exp_dir", type=str, default="logs")
+    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--num_epoch", type=int, default=50)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--seed", type=int, default=2024)
+    # optional: additional model hyperparamters
+    #parser.add_argument("--num_blocks", type=int, default=1)
+    # pass all arguments to train
+    train(**vars(parser.parse_args()))
